bcaad14ee1fccc9617e2d1b4b19c9aca9894cd8a
2dengyu2 <649355204@qq.com>
2024-01-11 20:52:17 +0800

    feat: 三个正方体

--- main.js
+++ main.js
@@ -1,38 +1,72 @@
 import * as THREE from 'three'
-import WebGL from 'three/addons/capabilities/WebGL.js'
 
-const renderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
-
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500)
-camera.position.set(0, 0, 100)
-camera.lookAt(0, 0, 0)
-
+const canvas = document.querySelector('#c')
+const renderer = new THREE.WebGLRenderer({ antialias: true, canvas })
+const camera = getCamera()
+camera.position.z = 2
 const scene = new THREE.Scene()
-
-// 材质
-const material = new THREE.LineBasicMaterial({ color: 0x0000ff })
-
 // 几何体
-const points = []
-points.push(new THREE.Vector3(-10, 0, 0))
-points.push(new THREE.Vector3(0, 10, 0))
-points.push(new THREE.Vector3(10, 0, 0))
+const geometry = getGeometry()
+generateLight()
 
-const geometry = new THREE.BufferGeometry().setFromPoints(points)
+function main () {
+  const cubes = [
+    makeInstance(geometry, 0x44aa88, 0),
+    makeInstance(geometry, 0x8844aa, -2),
+    makeInstance(geometry, 0xaa8844, 2)
+  ]
 
-const line = new THREE.Line(geometry, material)
-scene.add(line)
+  function render (time) {
+    time *= 0.001  // 将时间单位变为秒
 
-function animate () {
-  requestAnimationFrame(animate)
-  renderer.render(scene, camera)
+    cubes.forEach((cube, ndx) => {
+      const speed = 1 + ndx * .1
+      const rot = time * speed
+      cube.rotation.x = rot
+      cube.rotation.y = rot
+    })
+
+    renderer.render(scene, camera)
+
+    requestAnimationFrame(render)
+  }
+
+  requestAnimationFrame(render)
 }
 
-if (WebGL.isWebGLAvailable()) {
-  animate()
-} else {
-  const warning = WebGL.getWebGLErrorMessage()
-  document.getElementById('container').appendChild(warning)
+main()
+
+function makeInstance (geometry, color, x) {
+  const material = new THREE.MeshPhongMaterial({ color })
+
+  const cube = new THREE.Mesh(geometry, material)
+  scene.add(cube)
+
+  cube.position.x = x
+
+  return cube
+}
+
+function generateLight () {
+  const color = 0xFFFFFF
+  const intensity = 3
+  const light = new THREE.DirectionalLight(color, intensity)
+  light.position.set(-1, 2, 4)
+
+  scene.add(light)
+}
+
+function getGeometry () {
+  const boxWidth = 1
+  const boxHeight = 1
+  const boxDepth = 1
+  return new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth)
+}
+
+function getCamera () {
+  const fov = 75
+  const aspect = 2  // 相机默认值
+  const near = 0.1
+  const far = 5
+  return new THREE.PerspectiveCamera(fov, aspect, near, far)
 }
