c96198001eb8788ba10b59c8d64cc9254f80c93f
2dengyu2 <649355204@qq.com>
2024-01-14 20:53:34 +0800

    feat: 光照

--- main.js
+++ main.js
@@ -1,19 +1,24 @@
 import * as THREE from 'three'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js'
 import Stats from 'three/examples/jsm/libs/stats.module'
 
 const stats = new Stats()
 stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
 document.body.appendChild(stats.dom)
+const planeSize = 40
 
 const canvas = document.querySelector('#c')
-const gui = new GUI()
 const renderer = new THREE.WebGLRenderer({ antialias: true, canvas })
 const camera = getCamera()
 const scene = new THREE.Scene()
+
+const texture = loadTexture()
 // 几何体
 const objects = getGeometry()
-generateLight()
+const light = generateLight()
+// 添加控制器
+setControls()
 
 function main () {
 
@@ -26,10 +31,6 @@ function main () {
       camera.aspect = canvas.clientWidth / canvas.clientHeight
       camera.updateProjectionMatrix()
     }
-
-    objects.forEach((obj) => {
-      obj.rotation.y = time
-    })
 
     renderer.render(scene, camera)
 
@@ -54,114 +55,108 @@ function resizeRendererToDisplaySize (renderer) {
 
 function generateLight () {
   const color = 0xFFFFFF
-  const intensity = 500
+  const intensity = 150
   const light = new THREE.PointLight(color, intensity)
+  light.position.set(0, 10, 0)
   scene.add(light)
+  return light
 }
 
 function getGeometry () {
-
-  // 要更新旋转角度的对象数组
   const objects = []
-
-  // 一球多用
-  const radius = 1
-  const widthSegments = 50
-  const heightSegments = 50
-  const sphereGeometry = new THREE.SphereGeometry(
-    radius,
-    widthSegments,
-    heightSegments
-  )
-
-  // 太阳系场景
-  const solarSystem = new THREE.Object3D()
-  scene.add(solarSystem)
-  objects.push(solarSystem)
-  // 太阳
-  const sunMaterial = new THREE.MeshPhongMaterial({ emissive: 0xffff00 })
-  const sunMesh = new THREE.Mesh(sphereGeometry, sunMaterial)
-  sunMesh.scale.set(5, 5, 5) // 扩大太阳的大小
-  solarSystem.add(sunMesh)
-  objects.push(sunMesh)
-
-  // 地球轨道
-  const earthOrbit = new THREE.Object3D()
-  earthOrbit.position.x = 10
-  solarSystem.add(earthOrbit)
-  objects.push(earthOrbit)
-  // 地球
-  const earthMaterial = new THREE.MeshPhongMaterial({
-    color: 0x2233ff,
-    emissive: 0x112244
+  const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize)
+  const planeMat = new THREE.MeshPhongMaterial({
+    map: texture,
+    side: THREE.DoubleSide
   })
-  const earthMesh = new THREE.Mesh(sphereGeometry, earthMaterial)
-  earthOrbit.add(earthMesh)
-  objects.push(earthMesh)
-
-  // 月球轨道子场景
-  const moonOrbit = new THREE.Object3D()
-  moonOrbit.position.x = 2
-  earthOrbit.add(moonOrbit)
-  // 月球
-  const moonMaterial = new THREE.MeshPhongMaterial({ color: 0x888888, emissive: 0x222222 })
-  const moonMesh = new THREE.Mesh(sphereGeometry, moonMaterial)
-  moonMesh.scale.set(.5, .5, .5)
-  moonOrbit.add(moonMesh)
-  objects.push(moonMesh)
-
-  class AxisGridHelper {
-    constructor (node, units = 10) {
-      const axes = new THREE.AxesHelper()
-      axes.material.depthTest = false
-      axes.renderOrder = 2 // 在网格渲染之后再渲染
-      node.add(axes)
-
-      const grid = new THREE.GridHelper(units, units)
-      grid.material.depthTest = false
-      grid.renderOrder = 1
-      node.add(grid)
-
-      this.grid = grid
-      this.axes = axes
-      this.visible = false
-    }
-
-    get visible () {
-      return this._visible
-    }
-
-    set visible (v) {
-      this._visible = v
-      this.grid.visible = v
-      this.axes.visible = v
-    }
+  const mesh = new THREE.Mesh(planeGeo, planeMat)
+  mesh.rotation.x = Math.PI * -.5
+  scene.add(mesh)
+  objects.push(mesh)
+  {
+    const cubeSize = 4
+    const cubeGeo = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize)
+    const cubeMat = new THREE.MeshPhongMaterial({ color: '#8AC' })
+    const mesh = new THREE.Mesh(cubeGeo, cubeMat)
+    mesh.position.set(cubeSize + 1, cubeSize / 2, 0)
+    scene.add(mesh)
+    objects.push(mesh)
   }
-
-  function makeAxisGrid (node, label, units) {
-    const helper = new AxisGridHelper(node, units)
-    gui.add(helper, 'visible').name(label)
+  {
+    const sphereRadius = 3
+    const sphereWidthDivisions = 32
+    const sphereHeightDivisions = 16
+    const sphereGeo = new THREE.SphereGeometry(sphereRadius, sphereWidthDivisions, sphereHeightDivisions)
+    const sphereMat = new THREE.MeshPhongMaterial({ color: '#CA8' })
+    const mesh = new THREE.Mesh(sphereGeo, sphereMat)
+    mesh.position.set(-sphereRadius - 1, sphereRadius + 2, 0)
+    scene.add(mesh)
+    objects.push(mesh)
   }
-
-  makeAxisGrid(solarSystem, 'solarSystem', 25)
-  makeAxisGrid(sunMesh, 'sunMesh')
-  makeAxisGrid(earthOrbit, 'earthOrbit')
-  makeAxisGrid(earthMesh, 'earthMesh')
-  makeAxisGrid(moonOrbit, 'moonOrbit')
-  makeAxisGrid(moonMesh, 'moonMesh')
   return objects
 }
 
 function getCamera () {
-  const fov = 40
-  const aspect = 2 // the canvas default
+  const fov = 45
+  const aspect = 2  // canvas 的默认宽高 300:150
   const near = 0.1
-  const far = 1000
+  const far = 100
   const camera = new THREE.PerspectiveCamera(fov, aspect, near, far)
-  camera.position.set(0, 50, 0)
-  camera.up.set(0, 0, 1)
-  camera.lookAt(0, 0, 0)
+  camera.position.set(0, 10, 20)
+  const controls = new OrbitControls(camera, canvas)
+  controls.target.set(0, 5, 0)
+  controls.update()
   return camera
+}
+
+function loadTexture () {
+  const loader = new THREE.TextureLoader()
+  const texture = loader.load('public/resources/images/checker.png')
+  texture.wrapS = THREE.RepeatWrapping
+  texture.wrapT = THREE.RepeatWrapping
+  texture.magFilter = THREE.NearestFilter
+  texture.colorSpace = THREE.SRGBColorSpace
+  const repeats = planeSize / 2
+  texture.repeat.set(repeats, repeats)
+  return texture
+}
+
+function setControls () {
+  const helper = new THREE.PointLightHelper(light)
+  scene.add(helper)
+
+  class ColorGUIHelper {
+    constructor (object, prop) {
+      this.object = object
+      this.prop = prop
+    }
+
+    get value () {
+      return `#${this.object[this.prop].getHexString()}`
+    }
+
+    set value (hexString) {
+      this.object[this.prop].set(hexString)
+    }
+  }
+
+  function updateLight () {
+    helper.update()
+  }
+
+  const gui = new GUI()
+  gui.addColor(new ColorGUIHelper(light, 'color'), 'value').name('color')
+  gui.add(light, 'intensity', 0, 2, 0.01)
+  gui.add(light, 'distance', 0, 40).onChange(updateLight)
+  makeXYZGUI(gui, light.position, 'position', updateLight)
+}
+
+function makeXYZGUI (gui, vector3, name, onChangeFn) {
+  const folder = gui.addFolder(name)
+  folder.add(vector3, 'x', -10, 10).onChange(onChangeFn)
+  folder.add(vector3, 'y', 0, 10).onChange(onChangeFn)
+  folder.add(vector3, 'z', -10, 10).onChange(onChangeFn)
+  folder.open()
 }
 
 main()
